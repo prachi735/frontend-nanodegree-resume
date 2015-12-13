24a0d5dd832a937d8a5a50accbc44921648b63ab
prachi735 <prachi735@gmail.com>
2015-12-13 14:47:52 +0530

    First complete version of resume

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,3 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
@@ -26,7 +12,7 @@ var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</sp
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskillsStart = '<div class="list-group"> <h3 class="list-group-item-heading">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul></div>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
@@ -34,24 +20,24 @@ var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkDescription = '<p class="description" ><br>%data%</p>';
 
-var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectStart = '<div class="project-entry"><div class="flex-box"></div></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectDescription = '<p class="description"><br>%data%</p>';
+var HTMLprojectImage = '<a href="%dataImg%" rel="lightbox" data-lightbox="%dataTitle%" ><img src="%dataSmallImg%"></a>';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolDegree = '%data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineSchool = ' %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
@@ -60,47 +46,47 @@ var googleMap = '<div id="map"></div>';
 
 
 /*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
+ The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
+ */
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName() || function(){};
-    $('#name').html(iName);  
+      $('#name').html(iName);
   });
 });
 
 /*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
-*/
+ The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
+ */
 clickLocations = [];
 
 function logClicks(x,y) {
   clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
+      {
+          x: x,
+          y: y
+      }
   );
   console.log('x location: ' + x + '; y location: ' + y);
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+    console.log(logClicks(loc.pageX, loc.pageY));
 });
 
 
 
 /*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
-*/
+ This is the fun part. Here's where we generate the custom Google Map for the website.
+ See the documentation below for more details.
+ https://developers.google.com/maps/documentation/javascript/reference
+ */
 var map;    // declares a global map variable
 
 
 /*
-Start here! initializeMap() is called when page is loaded.
-*/
+ Start here! initializeMap() is called when page is loaded.
+ */
 function initializeMap() {
 
   var locations;
@@ -110,16 +96,16 @@ function initializeMap() {
   };
 
   /* 
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
-  */
+   For the map to be displayed, the googleMap var must be
+   appended to #mapDiv in resumeBuilder.js.
+   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
   /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
+   locationFinder() returns an array of every location string from the JSONs
+   written for bio, education, and work.
+   */
   function locationFinder() {
 
     // initializes an empty array
@@ -131,6 +117,7 @@ function initializeMap() {
     // iterates through school locations and appends each location to
     // the locations array
     for (var school in education.schools) {
+        console.log("school: " + education.schools[school].location);
       locations.push(education.schools[school].location);
     }
 
@@ -139,15 +126,15 @@ function initializeMap() {
     for (var job in work.jobs) {
       locations.push(work.jobs[job].location);
     }
-
+      console.log(locations);
     return locations;
   }
 
   /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
+   createMapMarker(placeData) reads Google Places search results to create map pins.
+   placeData is the object returned from search results containing information
+   about a single location.
+   */
   function createMapMarker(placeData) {
 
     // The next lines save location data from the search result object to local variables
@@ -185,9 +172,9 @@ function initializeMap() {
   }
 
   /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
+   callback(results, status) makes sure the search returned results for a location.
+   If so, it creates a new map marker for that location.
+   */
   function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       createMapMarker(results[0]);
@@ -195,9 +182,9 @@ function initializeMap() {
   }
 
   /*
-  pinPoster(locations) takes in the array of locations created by locationFinder()
-  and fires off Google place searches for each location
-  */
+   pinPoster(locations) takes in the array of locations created by locationFinder()
+   and fires off Google place searches for each location
+   */
   function pinPoster(locations) {
 
     // creates a Google place search service object. PlacesService does the work of
@@ -231,15 +218,15 @@ function initializeMap() {
 }
 
 /*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
+ Uncomment the code below when you're ready to implement a Google Map!
+ */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function (e) {
+//Make sure the map bounds get updated on page resize
+    map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,208 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name": "Prachi Singh",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile": "+91 8105 355261",
+        "email": "prachi735@gmail.com",
+        "github": "Prachi735",
+        "twitter": "@prachi735",
+        "location": "Bangalore"
+    },
+    "welcomeMessage": "",//TODO: add message
+    "skills": ["html", "css", "javascript", "jquery", "bootstrap", "angular.js", "node.js", "SharePoint", "SQL Server"],
+    "bioPic": "images/img.jpg",
+    "display": function () {
+        var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+
+        var formattedName = HTMLheaderName.replace('%data%', bio.name);
+        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+        $("#header").prepend(formattedRole).prepend(formattedName);
+        $("#header").prepend(formattedBioPic);
+        var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+        var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+        var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+        var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+        var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+        var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+
+        $("#topContacts").append(formattedMobile)
+            .append(formattedEmail)
+            .append(formattedGithub)
+            .append(formattedTwitter)
+            .append(location);
+
+
+        $("#header").append(HTMLskillsStart);
+        for (skill in bio.skills) {
+            var formattedSkills = HTMLskills.replace("%data%", bio.skills[skill]);
+            console.log(bio.skills[skill]);
+            $("#skills").append(formattedSkills);
+        }
+
+        $("#footerContacts").append(formattedMobile)
+            .append(formattedEmail)
+            .append(formattedGithub)
+            .append(formattedTwitter)
+            .append(location);
+
+    }
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "Pranveer Singh Institute of Technology",
+            "location": "Kanpur",
+            "degree": "B.Tech",
+            "majors": "Computer Science & Engineering",
+            "dates": "2007-2011",
+            "url": "http://www.psitcoe.ac.in/"
+        }
+    ],
+    "onlineCourses": [{
+        "title": "Front-End Web Developer Nanodegree",
+        "school": "Udacity.com",
+        "dates": "2015",
+        "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+    },
+        {
+            "title": "An Introduction to Interactive Programming in Python",
+            "school": "Coursera.org",
+            "dates": "2013",
+            "url": "https://www.coursera.org/account/accomplishments/verify/DF7FRD29JB"
+        }
+    ],
+    "display": function () {
+        for (item in education.schools) {
+            $("#education").append(HTMLschoolStart);
+            var formattedName = HTMLschoolName.replace("%data%", education.schools[item].name).replace("#", education.schools[item].url);
+            var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[item].location);
+            var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[item].degree);
+            var formattedMajors = HTMLschoolMajor.replace("%data%", education.schools[item].majors);
+            var formattedDates = HTMLschoolDates.replace("%data%", education.schools[item].dates);
+
+            $(".education-entry:last").append(formattedName)
+                .append(formattedLocation)
+                .append(formattedDegree)
+                .append(formattedMajors)
+                .append(formattedDates);
+        }
+        $(".education-entry:last").append(HTMLonlineClasses);
+        for (item in education.onlineCourses) {
+            var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[item].title).replace("#", education.onlineCourses[item].url);
+            var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[item].school);
+            var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[item].dates);
+            $(".education-entry:last").append(formattedTitle)
+                .append(formattedDates)
+                .append(formattedSchool);
+        }
+    }
+};
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Edge Verve Systems",
+            "title": "Technology Analyst",
+            "location": "Bangalore, IN",
+            "dates": "Aug 2015 - Current",
+            "description": "My work is to provide technical support to the Unit Planning and Assurance Management team. It involves the following task:\
+                * Designing and developing dashboard SharePoint 2013 & SQL Server Technologies.\
+                * Database development and maintenance on SQL Server 2012.\
+                * SharePoint 2013 Administration.\
+                * Development and maintenance of custom solutions for SharePoint 2013 sites using Visual Studio and \
+                * Automation of reports and dashboards.\
+                * Create visualizations and building dashboards using Tableau.\
+                "
+        },
+        {
+            "employer": "Infosys Limited",
+            "title": "Technology Analyst",
+            "location": "Bangalore, IN",
+            "dates": "Jan 2015 - Jul 2015",
+            "description": "My work is to provide technical support to the Unit Planning and Assurance Management team. It involves the following task:\
+                * Designing and developing dashboard SharePoint 2013 & SQL Server Technologies.\
+                * Database development and maintenance on SQL Server 2012.\
+                * SharePoint 2013 Administration.\
+                * Development and maintenance of custom solutions for SharePoint 2013 sites using Visual Studio and \
+                * Automation of reports and dashboards."
+        },
+        {
+            "employer": "Infosys Limited",
+            "title": "System Developer",
+            "location": "Bangalore, IN",
+            "dates": "Jul 2011 - Dec 2014",
+            "description": "My work is to provide technical support to the Unit Planning and Assurance Management team. It involves the following task:\
+                * Understand the working of the team and provide them with technical solutions and support.\
+                * Designing and developing dashboard using SQL Server analysis service, integration service and reporting service.\
+                * Database development and maintenance on SQL Server 2008 R2.\
+                * Development and maintenance of SharePoint sites (on SharePoint 2010, 2007 and 2003) using Visual Studio and SharePoint designer\
+                * Configuration and administration of SharePoint 2010 Server.\
+                * Upgrading of SharePoint sites from 2007 to 2010 version\
+                * Documentation (SRS, Design Document, Test Cases, User Help)\
+                * Take backup of and restore the systems (SharePoint sites and SQL Server database)"
+        }
+    ],
+    "display": function displayWork() {
+        for (item in work.jobs) {
+            $("#workExperience").append(HTMLworkStart);
+            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[item].employer);
+            var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[item].title);
+            var formattedEmployerTitle = formattedEmployer + formattedTitle;
+            var formattedDates = HTMLworkDates.replace("%data%", work.jobs[item].dates);
+            var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[item].location);
+            var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[item].description);
+
+            $(".work-entry:last").append(formattedEmployerTitle)
+                .append(formattedDates)
+                .append(formattedLocation)
+                .append(formattedDescription);
+        }
+    }
+};
+
+var projects = {
+    projects: [{
+        "title": "Online Interactive Resume",
+        "dates": "Dec 2015",
+        "description": "The project dynamically builds a resume using javascript, html & css. It also embeds a google map.",
+        "imagesSrc": ["images/frontend-nanodegree-resume-1.png", "images/frontend-nanodegree-resume-2.png", , "images/frontend-nanodegree-resume-3.png"],
+        "smallImagesSrc": ["images/frontend-nanodegree-resume-1-100x.png", "images/frontend-nanodegree-resume-2-100x.png", , "images/frontend-nanodegree-resume-3-100x.png"]
+    }
+    ],
+    "display": function () {
+        $("#projects").append(HTMLprojectStart);
+        for (item in projects.projects) {
+            var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[item].title);
+            var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[item].dates);
+            var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[item].description);
+            $(".project-entry").prepend(formattedDescription)
+                .prepend(formattedDates)
+                .prepend(formattedTitle);
+
+            for (itemImg in projects.projects[item].imagesSrc) {
+                var formattedImage = HTMLprojectImage.replace(/%dataImg%/g, projects.projects[item].imagesSrc[itemImg]);
+                formattedImage = formattedImage.replace(/%dataTitle%/g, projects.projects[item].title);
+                formattedImage = formattedImage.replace(/%dataSmallImg%/g, projects.projects[item].smallImagesSrc[itemImg]);
+                $(".project-images").append(formattedImage);
+            }
+        }
+    }
+};
+
+
+education.display();
+work.display();
+projects.display();
+bio.display();
+
+$("#mapDiv").append(googleMap);
+function inName(_name) {
+    var nameArray = name.trim().split(" ");
+    var firstName = nameArray[0];
+    var lastName = nameArray[1];
+    return firstName.slice(0, 1).toUpperCase() + firstName.slice(1).toLowerCase() + " " + lastName.toUpperCase();
+}
+
+//$("#main").append(internationalizeButton);
